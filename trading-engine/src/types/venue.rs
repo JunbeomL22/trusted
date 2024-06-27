aaca7ca9e59cde6b701a6527995b7f3cda7fe17f
Junbeom Lee <junbeoml22@gmail.com>
2024-06-27 17:10:25 +0900

    add indexing

--- trading-engine/src/types/venue.rs
+++ trading-engine/src/types/venue.rs
@@ -1,11 +1,22 @@
 use crate::types::venues::{
-    krx::KRX,
-    mock::Mock,
+    krx::{
+        KRX,
+        KrxOrderId,
+        KrxTraderId,
+        KrxAccountId,
+    },
+    mock_exchange::{
+        Mock,
+        MockOrderId,
+        MockTraderId,
+        MockAccountId,
+    },
 };
 use enum_dispatch::enum_dispatch;
+use serde::{Serialize, Deserialize};
 
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Hash)]
 #[enum_dispatch(VenueTrait)]
 pub enum Venue {
     Mock(Mock),
@@ -21,4 +32,36 @@ pub trait VenueTrait {
     fn check_trader_id(&self, _: &str) -> bool {
         unimplemented!("check_trader_id not implemented")
     }
+
+    fn check_order_id(&self, _: &str) -> bool {
+        unimplemented!("check_order_id not implemented")
+    }
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq, Eq, PartialOrd, Ord, Copy)]
+pub enum OrderId {
+    MockOrderId(MockOrderId),
+    KrxOrderId(KrxOrderId),
+}
+
+impl PartialEq<u64> for OrderId {
+    fn eq(&self, other: &u64) -> bool {
+        match self {
+            OrderId::MockOrderId(id) => id == other,
+            OrderId::KrxOrderId(id) => id == other,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Hash)]
+pub enum AccountId {
+    MockAccountId(MockAccountId),
+    KrxAccountId(KrxAccountId),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Hash)]
+pub enum TraderId {
+    MockTraderId(MockTraderId),
+    KrxTraderId(KrxTraderId),
+}
+
