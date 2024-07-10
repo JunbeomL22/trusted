bbcdfe848185a25f37e1c3baf64d478fe7a58b9b
Junbeom Lee <junbeoml22@gmail.com>
2024-07-10 18:39:57 +0900

    nil

---
+++ examples/parser/src/main.rs
@@ -0,0 +1,11 @@
+fn main () {
+    let x: u64 = 1;
+    println!("{:?}", x.to_le_bytes());
+    println!("{:?}", (x << 16).to_le_bytes());
+    println!("{:?}", ((x << 32) >> 16).to_le_bytes());
+
+    let x: u64 = 2_f32.powi(63) as u64;
+    println!("{:?}", x.to_le_bytes());
+    println!("{:?}", (x >> 16).to_le_bytes());
+    println!("{:?}", ((x >> 32) << 16).to_le_bytes());
+}
