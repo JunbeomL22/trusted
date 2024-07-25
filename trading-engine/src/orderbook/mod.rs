c96d56ac6ba3238aa7c4534c326a6309fb1195c4
Junbeom Lee <junbeoml22@gmail.com>
2024-07-25 18:41:15 +0900

    nil

--- trading-engine/src/orderbook/mod.rs
+++ trading-engine/src/orderbook/mod.rs
@@ -1,3 +1,16 @@
 pub mod half_book;
 pub mod level;
-pub mod book;
+
+use crate::orderbook::half_book::HalfBook;
+use crate::types::{
+    isin_code::IsinCode,
+    venue::Venue,
+};
+
+#[derive(Debug, Clone)]
+pub struct OrderBook {
+    pub bids: HalfBook,
+    pub asks: HalfBook,
+    isin_code: IsinCode,
+    venue: Venue,
+}
